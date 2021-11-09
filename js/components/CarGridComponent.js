1dbb81cd108dd21e1b833065bfefec4f25027111
Nojus Jankevičius <1nojus.jankevicius@gmail.com>
2021-11-09 15:05:12 +0200

    [7]. loading card element template

--- js/components/CarGridComponent.js
+++ js/components/CarGridComponent.js
@@ -23,9 +23,28 @@ class FurnitureGridComponent {
 
     render = () => {
         if (this.state.cars.length === 0) {
-          this.htmlElement.innerHTML = `<div class="text-center">siunčiama...</div>`;
+          this.htmlElement.innerHTML = `<div class="text-center"><img src="assets/loading.gif" /></div>`;
         } else {
-          this.htmlElement.innerHTML = `<div class="text-center">parsiųsta!</div>`
+          this.htmlElement.innerHTML = `        <div class="card card shadow position-relative">
+          <img src="https://m.media-amazon.com/images/I/71VEtPLgBxL._AC_SX569_.jpg"  height="300px" class="card-img-top">
+          <div class="card-body ">
+              <h5 class="card-title">opel žopel</h5>
+              <div>
+                  <span>Year:</span>
+                  2000
+              </div>
+              <div>
+                  <span>Fuel type:</span>
+                  gas
+              </div>
+              <div>
+                  <span>Price:</span>
+                  600
+              </div>
+              <button class="btn btn-danger btn-sm position-absolute top-0 end-0 mt-2 me-2">✕</button>
+          </div>
+      </div>
+`
 
         }
     }
